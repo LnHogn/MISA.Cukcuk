1b4962be619cd246d7f98c2fb4259548f371e723
LnHognnn <117805743+LnHogn@users.noreply.github.com>
2024-07-04 14:46:23 +0700

    UI

---
+++ UI/js/employee.js
@@ -0,0 +1,24 @@
+document.getElementById("btnClose").addEventListener("click", function() {
+    var dialog = document.getElementsByClassName("m-dialog");
+    dialog[0].style.display = "none"; 
+});
+
+document.getElementById("btnAdd").addEventListener("click", function() {
+    var dialog = document.getElementsByClassName("m-dialog");
+    dialog[0].style.display = "block"; 
+    document.getElementById('ma-nhan-vien').focus();
+});
+
+document.getElementById("btnClose-edit").addEventListener("click", function() {
+    var dialog = document.getElementsByClassName("m-dialog m-dialog-edit");
+    dialog[0].style.display = "none"; 
+});
+
+document.getElementById("btn-sua").addEventListener("click", function() {
+    var dialog = document.getElementsByClassName("m-dialog m-dialog-edit");
+    dialog[0].style.display = "block"; 
+    document.getElementById('ma-nhan-vien').focus();
+});
+
+
+  
