ea5053cd9e2e5e6e420b101197113f7b8f097a7d
LnHognnn <117805743+LnHogn@users.noreply.github.com>
2024-07-06 11:26:50 +0700

    update table

--- UI/js/employee.js
+++ UI/js/employee.js
@@ -14,7 +14,7 @@ document.getElementById("btnClose-edit").addEventListener("click", function() {
     dialog[0].style.display = "none"; 
 });
 
-document.getElementById("btn-sua").addEventListener("click", function() {
+document.getElementsByClassName("btn-edit").addEventListener("click", function() {
     var dialog = document.getElementsByClassName("m-dialog m-dialog-edit");
     dialog[0].style.display = "block"; 
     document.getElementById('ma-nhan-vien').focus();
